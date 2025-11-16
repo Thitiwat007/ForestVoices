62c3329879763e75421a56326ef2b9dccec1e101
Thitiwat007 <stu45511@samakkhi.ac.th>
2025-11-16 17:29:27 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css.css" class="css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<style>
+    .box{
+        position: relative;
+    }
+    .container{
+        overflow: visible;
+    }
+    table{
+        max-width: 1150px;
+    }
+</style>
+<body><br><br><div class="box container">
+    <center><table border="0">
+        <td>
+           <h1 align="center">มรดกป่าไม้</h1>
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           ระบบปัญญาประดิษฐ์เพื่อการเฝ้าระวังและแจ้งเตือนภัยจากการเผาป่าและการทำลายป่า 
+           พร้อมรับมือกับมลพิษ PM 2.5 เพื่อการอนุรักษ์อย่างยั่งยืนของชุมชน<br><br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           จังหวัดเชียงรายประสบปัญหาไฟป่า การทำลายป่า และมลพิษ PM2.5 โดยเฉพาะในฤดูแล้ง สาเหตุหลักมาจากการเผาป่าและการเผาในที่โล่ง 
+           ส่งผลกระทบต่อสุขภาพประชาชนและการท่องเที่ยว ปัจจุบันยังขาดเครื่องมือเฝ้าระวังที่รวดเร็วและมีประสิทธิภาพ โครงการนี้จึงเสนอการใช้เทคโนโลยีปัญญาประดิษฐ์ (AI) 
+           ร่วมกับระบบตรวจจับและเซนเซอร์วัดฝุ่น เพื่อเฝ้าระวังไฟป่าและคาดการณ์ PM2.5 แบบเรียลไทม์ ช่วยชุมชนตอบสนองได้ทันเหตุการณ์ 
+           และเป็นองค์ความรู้ด้านเทคโนโลยีเพื่อการอนุรักษ์ป่าและคุณภาพอากาศอย่างยั่งยืน<br><br>
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+        </td>
+    </table></center></div>
+</body>    
+</html>
