fa9e62e2242c3e5d595a99a04d1f9b96b2088db3
Thitiwat007 <stu45511@samakkhi.ac.th>
2025-11-16 20:42:39 +0700

    Update y.js

--- y.js
+++ y.js
@@ -122,3 +122,12 @@ document.querySelectorAll(".dropdown .dropbtn").forEach(button => {
         this.parentElement.classList.toggle("active");
     });
 });
+
+function toggleMenu() {
+    let menu = document.getElementById("mobileMenu");
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
